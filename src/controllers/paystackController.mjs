8009b0b9a253cb6ba3bce357900c618fb6188c0e
AugustineLove <augustinelovestephens@gmail.com>
2025-04-10 14:36:57 +0000

    Changed localhost endpoint

--- src/controllers/paystackController.mjs
+++ src/controllers/paystackController.mjs
@@ -16,7 +16,7 @@ export const initializePayment = async (req, res) => {
                 email,
                 amount: amount * 100, // Convert to kobo
                 currency: "GHS",
-                callback_url: "http://localhost:5173/paymentCallback",
+                callback_url: "https://myward.tech/paymentCallback",
             },
             {
                 headers: {
