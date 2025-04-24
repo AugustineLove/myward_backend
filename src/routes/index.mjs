349e5614d7a37930beafe4dcd66ceed82314dde3
AugustineLove <augustinelovestephens@gmail.com>
2025-04-24 08:53:52 +0000

    Update

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -7,8 +7,6 @@ import transactionRoutes from "./transactionRoutes.mjs";
 import parentRoutes from "./parentRoutes.mjs";
 import paystackRoutes from "./paystackRoutes.mjs";
 import helpRoutes from "./helpRoutes.mjs";
-// import transactionRoutes from "./transactionRoutes.mjs";
-// import paystackRoutes from "./paystackRoutes.mjs";
 const router = Router();
 
 router.use('/api/schools', schoolRoutes); 
--- src/routes/parentRoutes.mjs
+++ src/routes/parentRoutes.mjs
@@ -1,13 +1,3 @@
-// import { Router } from "express";
-// import { getAllChildren, sendParentMessage, verifyParentNumber } from "../controllers/parentController.mjs";
-
-// const parentRoutes = Router();
-
-// parentRoutes.get("/:parentNumber", getAllChildren)
-// parentRoutes.post("/verify", verifyParentNumber)
-// parentRoutes.post('/sendMessage', sendParentMessage)
-// export default parentRoutes;
-
 
 import { Router } from "express";
 import { addParentEmailToStudents, getAllChildren, sendParentMessage, verifyParentNumber } from "../controllers/parentController.mjs";
